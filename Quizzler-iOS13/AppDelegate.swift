4c8caf6fe4c2c8d6f70acced4bc1c4f29bab14c9
EleanorKal <eleanor@blarnya.com>
2021-04-07 19:51:32 +0200

    Latest Updates

--- Quizzler-iOS13/AppDelegate.swift
+++ Quizzler-iOS13/AppDelegate.swift
@@ -2,8 +2,8 @@
 //  AppDelegate.swift
 //  Quizzler-iOS13
 //
-//  Created by Angela Yu on 12/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by Eleanor Kalu on 3 April 2021.
+//  Copyright © 2021 Blarnya. All rights reserved.
 //
 
 import UIKit
---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Eleanor Kalu on 3 April 2021.
+//  Copyright © 2021 Blarnya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    var quizBrain = QuizBrain()
+        
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // loads the first question
+        updateUI()
+        
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle! // True, or False
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+//        print(sender.currentTitle ?? "")
+//        print(userAnswer)
+        
+
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+         
+        // safetyCheck()
+        quizBrain.nextQuestion()
+        
+        let seconds = 0.3
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            // Put your code which should be executed with a delay here
+            self.updateUI()
+        }
+    
+    }
+        
+    func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+    }
+    
+    
+}
+
